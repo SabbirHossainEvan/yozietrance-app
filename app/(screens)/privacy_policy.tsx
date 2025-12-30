b8317f949652a1bc1929e054efa9e46718381838
93891396soriful@gmail.com <93891396soriful@gmail.com>
2025-12-31 00:00:51 +0600

    added the terms of service screen

--- app/(screens)/privacy_policy.tsx
+++ app/(screens)/privacy_policy.tsx
@@ -10,10 +10,7 @@ const PrivacyPolicy = () => {
     };
 
     return (
-        // FIX 1: Add flex: 1 to SafeAreaView
-        <SafeAreaView style={{ flex: 1}}>
-
-            {/* FIX 2: Add flex: 1 to this wrapper View */}
+        <SafeAreaView style={{ flex: 1 }}>
             <View style={{ flex: 1 }}>
 
                 {/* Header */}
@@ -32,24 +29,20 @@ const PrivacyPolicy = () => {
                 </View>
 
                 {/* Body */}
-                {/* FIX 3: Ensure ScrollView has flex: 1 (it does by default, but parent must be constrained) */}
                 <ScrollView
                     style={{ flex: 1 }}
                     contentContainerStyle={{ paddingHorizontal: 20, paddingTop: 20, paddingBottom: 40 }}
                     showsVerticalScrollIndicator={false}
                 >
                     <View>
-                        {/* Main Title */}
                         <Text style={{ fontSize: 16, fontWeight: '700', color: '#1F2937', marginBottom: 20 }}>
                             Privacy & Policy
                         </Text>
 
-                        {/* Main Description */}
                         <Text style={{ fontSize: 16, color: '#4B5563', lineHeight: 24, marginBottom: 30 }}>
                             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
                         </Text>
 
-                        {/* Section 1 */}
                         <View style={{ marginBottom: 25 }}>
                             <Text style={{ fontSize: 16, fontWeight: '700', color: '#1F2937', marginBottom: 12 }}>
                                 1. Information we collect
@@ -59,7 +52,6 @@ const PrivacyPolicy = () => {
                             </Text>
                         </View>
 
-                        {/* Section 2 */}
                         <View style={{ marginBottom: 25 }}>
                             <Text style={{ fontSize: 16, fontWeight: '700', color: '#1F2937', marginBottom: 12 }}>
                                 2. How We Use Your Information
@@ -69,7 +61,6 @@ const PrivacyPolicy = () => {
                             </Text>
                         </View>
 
-                        {/* Section 3 */}
                         <View style={{ marginBottom: 40 }}>
                             <Text style={{ fontSize: 16, fontWeight: '700', color: '#1F2937', marginBottom: 12 }}>
                                 3. Information Sharing
